752371fc09ee0178f8b957a0e0abe4b5f6752ce9
KodeKing22 <jonkylemars@yahoo.com>
2022-07-21 14:58:14 -0500

    Created Add Guest Page folder and file in Pages folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import AddGuestPage from "./pages/AddGuestPage/AddGuestPage";
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
@@ -29,6 +30,14 @@ function App() {
         />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/addguest"
+          element={
+            <PrivateRoute>
+              <AddGuestPage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
       <Footer />
     </div>
---
+++ frontend/src/pages/AddGuestPage/AddGuestPage.js
@@ -0,0 +1,135 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import useCustomForm from "../../hooks/useCustomForm";
+import axios from "axios";
+
+let initialValues = {
+  first_name: "",
+  last_name: "",
+  email_address: "",
+  phone_number: "",
+  address: "",
+  city: "",
+  zip_code: "",
+  state: "",
+  date_of_visit: "",
+  guest_of: "",
+};
+
+const AddGuestPage = () => {
+  const [user, token] = useAuth();
+  const navigate = useNavigate();
+  const [formData, handleInputChange, handleSubmit] = useCustomForm(
+    initialValues,
+    addNewGuest
+  );
+
+  async function addNewGuest() {
+    try {
+      let response = await axios.post(
+        "http://127.0.0.1:8000/api/guests/",
+        formData,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  return (
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
+        <label>
+          First Name:{""}
+          <input
+            type="text"
+            name="first_name"
+            value={formData.first_name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Last Name:{""}
+          <input
+            type="text"
+            name="last_name"
+            value={formData.last_name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Email Address:{""}
+          <input
+            type="text"
+            name="email_address"
+            value={formData.email_address}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Phone Number:{""}
+          <input
+            type="text"
+            name="phone_number"
+            value={formData.phone_number}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Address:{""}
+          <input
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          City:{""}
+          <input
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          State:{""}
+          <input
+            type="text"
+            name="state"
+            value={formData.state}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Zip Code:{""}
+          <input
+            type="text"
+            name="zip_code"
+            value={formData.zip_code}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Guest of:{""}
+          <input
+            type="text"
+            name="guest_of"
+            value={formData.guest_of}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button>Add Guest</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddGuestPage;
