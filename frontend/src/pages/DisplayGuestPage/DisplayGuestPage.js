b23bda792415ce88b55b4da80c8dcb69ea3e5ccf
KodeKing22 <jonkylemars@yahoo.com>
2022-07-21 16:10:34 -0500

    Created a Display Guest Page to Display all new guest

---
+++ frontend/src/pages/DisplayGuestPage/DisplayGuestPage.js
@@ -0,0 +1,19 @@
+
+
+
+const DisplayGuestPage = () => {
+    return ( 
+        <div>
+            {}
+            return(
+                <>
+                <p>{guest.first_name}</p>
+                <p>{guest.last_name}</p>
+                <p>{guest.guest_of}</p>
+                </>
+            )
+        </div>
+     );
+}
+ 
+export default DisplayGuestPage;
